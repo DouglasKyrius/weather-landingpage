42d39ff1dea64c0db7e11ef7fe68d7d45f2a14fe
DouglasKyrius <douglaskyrius@gmail.com>
2020-07-04 22:07:14 -0400

    adding weather components and a search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Info from './Components/Info/Info';
+import Country from './Components/Country/Country';
 
 import './App.css'
 
+const api = {
+  key: 'c88e4b3fb0e4235c997fb8cb98ff1a40',
+  base: 'http://api.openweathermap.org/data/2.5/',
+}
+
 function App() {
   let [tempSymbol, setTempSymbol] = useState('C');
+  let [cities, setCities] = useState([]);
+  let [cityOne, setCityOne] = useState({});
+  let [queryOne, setQueryOne] = useState('');
+  let [cityTwo, setCityTwo] = useState({});
+  let [queryTwo, setQueryTwo] = useState('');
+
+  useEffect(() => {
+    (async function fetchData() {
+      const response = await fetch(`${api.base}group?id=605155,3451190&units=metric&appid=${api.key}`)
+      const data = await response.json();
+      setCities(data.list);
+    })();  
+  }, []);
+
+  const searchOne = async event => {
+    if (queryOne !== '' && event.key === "Enter") {
+      const response = await fetch(`${api.base}weather?q=${queryOne}&units=metric&APPID=${api.key}`)
+      const data = await response.json();
+      setCityOne(data);
+      setQueryOne('');
+    }
+  }
+  
+  const searchTwo = async event => {
+    if (event.key === "Enter") {
+      const response = await fetch(`${api.base}weather?q=${queryTwo}&units=metric&APPID=${api.key}`)
+      const data = await response.json();
+      setCityTwo(data);
+      setQueryTwo('');
+    }
+  }
+
   return (
     <div className="app">
       <main>
         <Info tempSymbol={tempSymbol} setTempSymbol={() => setTempSymbol(tempSymbol === 'C' ? 'F' : 'C')} />
+        <Country defaultCities={cities[0]} tempSymbol={tempSymbol} city={cityOne} query={queryOne} setQuery={setQueryOne} search={searchOne} />
+        <Country defaultCities={cities[1]} tempSymbol={tempSymbol} city={cityTwo} query={queryTwo} setQuery={setQueryTwo} search={searchTwo} />
       </main>
     </div>
   );
---
+++ src/Components/Country/Country.js
@@ -0,0 +1,107 @@
+import React from 'react';
+
+import sunIcon from '../../img/sun-icon.svg';
+import snowIcon from '../../img/snow-icon.svg';
+import warm from '../../img/pic-1.jpg';
+import cold from '../../img/pic-2.jpg';
+
+import './Country.css';
+
+const Country = ({defaultCities, tempSymbol, ...props}) => {
+  console.log(defaultCities);
+  console.log(props.city);
+  
+  let currentTemp = 0;
+
+  if (props.city.main) {
+    tempSymbol === 'C' 
+      ? currentTemp = props.city.main.temp 
+      : currentTemp = (props.city.main.temp * 9/5) + 32;
+  } else if (defaultCities) {
+    tempSymbol === 'C' 
+      ? currentTemp = defaultCities.main.temp 
+      : currentTemp = (defaultCities.main.temp * 9/5) + 32;
+  }
+  
+
+  return ( 
+    <section>
+      { props.city.main ?
+          <React.Fragment>
+            <div className="floater">
+              <div className="icon-temp">
+                {props.city.main.temp >= 16 ?
+                  <img src={sunIcon} alt="Sun icon" />
+                  :
+                  <img src={snowIcon} alt="Cold icon" />
+                }
+                <p id="temp"> {Math.round(currentTemp)}° </p>
+              </div>
+              <p>{props.city.weather[0].main.toUpperCase()}</p>
+            </div>
+            {props.city.main.temp >= 16 ?
+              <img src={warm} alt="Warm image" id="country-img" />
+              :
+              <img src={cold} alt="Cold image" id="country-img" />
+            }
+            <div className="country">
+              <div>
+                <input 
+                  type="text"
+                  className="search-bar"
+                  placeholder="Search..."
+                  onChange={e => props.setQuery(e.target.value)}
+                  value={props.query}
+                  onKeyPress={props.search}
+                />
+              </div>
+              <div>
+                <p>{props.city.name}</p>
+                <p>{props.city.sys.country}</p>
+              </div>
+            </div>
+          </React.Fragment>
+        :
+          defaultCities ?
+            <React.Fragment>
+              <div className="floater">
+                <div className="icon-temp">
+                  {defaultCities.main.temp >= 16 ?
+                    <img src={sunIcon} alt="Sun icon" />
+                    :
+                    <img src={snowIcon} alt="Cold icon" />
+                  }
+                  <p id="temp"> {Math.round(currentTemp)}° </p>
+                </div>
+                <p>{defaultCities.weather[0].main.toUpperCase()}</p>
+              </div>
+              {defaultCities.main.temp >= 16 ?
+                <img src={warm} alt="Warm image" id="country-img" />
+                :
+                <img src={cold} alt="Cold image" id="country-img" />
+              }
+              <div className="country">
+                <div>
+                  <input 
+                    type="text"
+                    className="search-bar"
+                    placeholder="Search..."
+                    onChange={e => props.setQuery(e.target.value)}
+                    value={props.query}
+                    onKeyPress={props.search}
+                  />
+                </div>
+                <div>
+                  <p>{defaultCities.name}</p>
+                  <p>{defaultCities.sys.country}</p>
+                </div>
+              </div>
+            </React.Fragment>
+          :
+          ''
+      }
+    </section>
+  );
+}
+
+export default Country;
